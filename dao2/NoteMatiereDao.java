d7abc5256a48c5836965b75629c87483637c2205
BOUZDIG Zakaria <zakariabouzdig@gmail.com>
2025-04-13 00:45:00 +0200

    initial commit

---
+++ dao2/NoteMatiereDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao2;
+
+import entities.NoteMatiere;
+
+public class NoteMatiereDao extends AbstractDao<NoteMatiere> {
+    public NoteMatiereDao() {
+        super(NoteMatiere.class);
+    }
+}
